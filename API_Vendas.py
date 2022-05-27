a84693676bc3e7c00b3ce39a6e9c71e827699e79
FelipeM0reira <felipeemooreira@gmail.com>
2022-05-26 23:29:12 -0300

    Continuação do projeto

--- API_Vendas.py
+++ API_Vendas.py
@@ -1,7 +1,15 @@
 import pandas as pd
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-tabela = pd.read_csv('criando API no Python.csv')
-print(tabela)
+
+@app.route('/')
+def homepage():
+    tabela = pd.read_csv('criando API no Python.csv')
+    total_vendas = tabela['Vendas'].sum()
+    resposta = {'total_vendas': total_vendas}
+    return jsonify(resposta)
+
+
+app.run(host='0.0.0.0')
