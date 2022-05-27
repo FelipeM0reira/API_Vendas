dd5b1b67bb10fd9a05040bb18ab0a172079440aa
FelipeM0reira <felipeemooreira@gmail.com>
2022-05-27 19:13:04 -0300

    API local pronta.

--- API_Vendas.py
+++ API_Vendas.py
@@ -8,7 +8,11 @@ app = Flask(__name__)
 def homepage():
     tabela = pd.read_csv('criando API no Python.csv')
     total_vendas = tabela['Vendas'].sum()
-    resposta = {'total_vendas': total_vendas}
+    total_vendas_tv = tabela['TV'].sum()
+    total_vendas_radio = tabela['Radio'].sum()
+    total_vendas_jornal = tabela['Jornal'].sum()
+    resposta = {'total_vendas': total_vendas}, {
+        'total_vendas_tv': total_vendas_tv}, {'total_vendas_radio': total_vendas_radio}, {'total_vendas_jornal': total_vendas_jornal}
     return jsonify(resposta)
 
 
